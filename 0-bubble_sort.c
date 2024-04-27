d5db9c72d4e2836962b06c4444f6640527b1ca55
Flames <isaacomenuche@gmail.com>
2024-04-27 11:12:20 +0100

    add swap func in bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,22 @@
 #include "sort.h"
 
 /**
+ * swap_array - swaps two elements in an array
+ *
+ * @array: The array to manipulate
+ * @index_1: The index of the first element to swap
+ * @index_2: The index of the second element to swap
+ */
+void swap_array(int *array, size_t index_1, size_t index_2)
+{
+	int tmp = array[index_1];
+
+	array[index_1] = array[index_2];
+	array[index_2] = tmp;
+}
+
+
+/**
  * bubble_sort - sorts and print an array of integers in ascending order
  *
  * @array: array of integers
